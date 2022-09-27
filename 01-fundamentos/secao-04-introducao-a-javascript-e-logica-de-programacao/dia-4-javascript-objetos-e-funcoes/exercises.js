4dbcee1444888bb301660c4112fe547f6b0719af
Joice Nicolau <joice_nicolau@hotmail.com>
2022-09-26 23:11:39 -0300

    Resolvendo exercício 2 -parte 2

--- 01-fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises.js
+++ 01-fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises.js
@@ -88,7 +88,25 @@ let leitor = {
 
   console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos.titulo);
 
+/* Exercício 2 */
 
+let leitor2 = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+      {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      }
+    ],
+  };
 
 
 
