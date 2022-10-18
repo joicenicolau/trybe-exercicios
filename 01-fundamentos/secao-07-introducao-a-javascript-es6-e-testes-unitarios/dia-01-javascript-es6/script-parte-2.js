401513228cc1be8f57474ca9c9f3ff2fcf24b32c
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-17 21:44:02 -0300

    Resolvendo exercícios 2 - parte II

--- 01-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6/script-parte-2.js
+++ 01-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6/script-parte-2.js
@@ -13,3 +13,29 @@ const factorial = (num) => {
 
 const result = factorial(4); // Ex. 4 = num.
 console.log(`Esse é o fatorial ${result}`);
+
+// Exercício 2
+
+// Crie uma função que receba uma frase como parâmetro e retorne a maior palavra contida nesta frase.
+
+const sizeWord = (text) => {
+  const wordText = text.split(' ');
+  let wordLength = 0;
+  let result = '';
+
+  for (const word of wordText) {
+      if (word.length > wordLength) {
+          wordLength = word.length;
+          result = word;
+      }
+  }
+
+  return result;
+}
+
+console.log(sizeWord('Ela escondia com palavras o que eu publicava com silêncio'));
+
+// Exercício 3
+// No HTML
+
+
