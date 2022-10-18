ff3fe91d00468d9b2bf500b9a4cc6bc77df48f3a
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-17 21:02:03 -0300

    Resolvendo requisito 1

--- 01-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6/script-parte-2.js
+++ 01-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6/script-parte-2.js
@@ -2,24 +2,14 @@
 
 // Crie uma função que receba um número e retorne seu fatorial.
 
-// const factorial = (num) => {
-//   let result = 1;
-//    result ? num === 1 : num !== 1;
-   
-//    for (let index = num -1; index >= 1; index += 1) {
-//     num = num * index;
-//    }
-//    return num;
-// }
+const factorial = (num) => {
+  let result = 1;
+      
+   for (let index = num; index > 1; index -= 1) {
+    result *= index;
+   }
+   return result;
+}
 
-// console.log(factorial(5));
-
-function factorial(num) {
-    if (num === 0 || num === 1)
-      return 1;
-    for (let i = num - 1; i >= 1; i -= 1) {
-      num *= i;
-    }
-    return num;
-  }
-  console.log(factorial(5));
+const result = factorial(4); // Ex. 4 = num.
+console.log(`Esse é o fatorial ${result}`);
