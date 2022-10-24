4e66c2360a0382c3c45a9bca959c67a9d3820bd8
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-24 15:50:36 -0300

    aula 7.3

---
+++ 01-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/teste/bonus.tests.js
@@ -0,0 +1,7 @@
+const searchEmployee = require('../src/bonus');
+
+describe('Teste Função searchEmployee', () => {
+  it('Verifica se a função existe', () => {
+   expect(searchEmployee).toBedefined();
+  });
+});
