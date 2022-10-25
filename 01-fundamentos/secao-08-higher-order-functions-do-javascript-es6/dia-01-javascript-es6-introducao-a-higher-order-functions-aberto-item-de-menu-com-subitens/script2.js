7e2a063a7c9e65fff61e79307a74523154a0c0b8
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-25 11:08:08 -0300

    Resolvendo exercícios 2 e 3

--- 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions-aberto-item-de-menu-com-subitens/script2.js
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions-aberto-item-de-menu-com-subitens/script2.js
@@ -2,7 +2,7 @@
 
 //Ex1. Nova pesoa contratada
 const fullName = (nomeCompleto) => {
-   const email = nomeCompleto.toLowerCase().replace(' ', '_');
+   const email = nomeCompleto.toLowerCase().replaceAll(' ', '_');
    return { nomeCompleto, email: `${email}@trybe.com` };
 };
  
@@ -14,4 +14,54 @@ const newEmployees = (callback) => {
     }
     return employees;
 };
-console.log(newEmployees(fullName));
+// console.log(newEmployees(fullName));
+
+
+// Ex2. Sorteador de loteria
+const resultadoLoteria = (meuNumero, numSorteado) => {
+    const geraResultado = Math.floor((Math.random() * 5) + 1);
+    return numSorteado(meuNumero, geraResultado);
+};
+
+const checkNumeros = (meuNumero, numSorteado) => (meuNumero === numSorteado) ? 'Parabéns você ganhou' : 'Tente novamente';
+
+// console.log(resultadoLoteria(2, checkNumeros)); // resultadoLoteria = 1 a 6, numbers. 
+
+
+// Ex3. Corretor automático de exame
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+// const comparaAnswers = (RIGHT_ANSWERS, STUDENT_ANSWERS) => {
+//     if(RIGHT_ANSWERS === STUDENT_ANSWERS) {
+//      return 1;
+//     } if (STUDENT_ANSWERS === 'N.A') {
+//      return 0;    
+//     } 
+//     return -0.5;    
+// };
+
+// const contaPontos = (RIGHT_ANSWERS, STUDENT_ANSWERS, finalPoints) => {
+//     let points = 0; 
+
+//     for (let index = 0; index < RIGHT_ANSWERS.length; index += 1) {
+//         const element = finalPoints(RIGHT_ANSWERS[index], STUDENT_ANSWERS[index]);
+//         points += element;
+//     }
+//     return `Resultado final: ${points} pontos`;
+// };
+
+// console.log(contaPontos(RIGHT_ANSWERS, STUDENT_ANSWERS, comparaAnswers));
+
+const compara = (corretas, respostas) => {
+    let pontos = 0;
+    for (let index = 0; index < corretas.length; index += 1) {
+        if(corretas[index] === respostas[index]) pontos += 1;
+        if (corretas[index] !== respostas[index] && respostas[index] !== 'N.A') pontos -= 0.5;
+        }
+        return `Resultado final: ${pontos} pontos`;
+    };
+
+const escola = (corretas, respostas, funçãoCompara) => funçãoCompara(corretas, respostas);
+    
+console.log(escola(RIGHT_ANSWERS, STUDENT_ANSWERS, compara));
