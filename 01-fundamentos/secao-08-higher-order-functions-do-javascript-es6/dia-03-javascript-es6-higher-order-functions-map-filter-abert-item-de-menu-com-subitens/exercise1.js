1a49a8265e152f2c7b2b3ab5cfbe6c3fe2907fdc
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-26 15:18:01 -0300

    Resolvendo exercício 1

--- 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-filter-abert-item-de-menu-com-subitens/exercise1.js
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-filter-abert-item-de-menu-com-subitens/exercise1.js
@@ -65,3 +65,16 @@ const books = [
 ];
   
 // Adicione o código do exercício aqui:
+// const expectedResult = [
+//     'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+//     'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+//     'Fundação - Ficção Científica - Isaac Asimov',
+//     'Duna - Ficção Científica - Frank Herbert',
+//     'A Coisa - Terror - Stephen King',
+//     'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+// ];
+  
+function formatedBookNames() {
+    return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+}
+// console.log(formatedBookNames());
