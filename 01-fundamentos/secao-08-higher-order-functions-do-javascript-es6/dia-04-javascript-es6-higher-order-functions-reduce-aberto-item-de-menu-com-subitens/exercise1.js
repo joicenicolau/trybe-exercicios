2a1060a2b63441aa793e43f37d3a5c4794e5a48d
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-27 16:10:09 -0300

    Iniciando dia 8.4

---
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce-aberto-item-de-menu-com-subitens/exercise1.js
@@ -0,0 +1,13 @@
+// Exercícios - agora, a prática:
+// Organizando biblioteca
+
+// Utilize o reduce para transformar uma matriz em um array.
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+  
+function flatten() {
+    // escreva seu código aqui
+}
---
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce-aberto-item-de-menu-com-subitens/exercise5.js
@@ -0,0 +1,13 @@
+// Exercicíos - agora, a prática:
+// Dado o array de nomes, utilize o reduce para retornar a quantidade de vezes em que aparece a letra a, maiúscula ou minúscula.
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+];
+  
+// const expectedResult = 20;
+  
+function containsA() {
+    // escreva seu código aqui
+}
