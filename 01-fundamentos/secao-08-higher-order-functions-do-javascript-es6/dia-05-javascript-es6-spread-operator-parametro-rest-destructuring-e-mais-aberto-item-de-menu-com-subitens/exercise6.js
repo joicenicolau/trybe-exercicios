6e9cc3420dfe9a19fb7fd5225599497e499191e5
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-28 18:05:43 -0300

    ex 6

--- 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/exercise6.js
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/exercise6.js
@@ -8,4 +8,4 @@ const chiron = ['Chiron', 'Bugatti', 2016];
 // escreva toObject abaixo
 const toObject = ( [ name, model, year ] ) => ( { name, model, year } )
 
-console.log(toObject(palio));
+console.log(toObject(palio))
