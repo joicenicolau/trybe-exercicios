8bcadf7ba1c4cc542184e599ecdfac62786f7af2
Joice Nicolau <joice_nicolau@hotmail.com>
2022-10-28 15:30:56 -0300

    Estruturando arquivos dos  exercícios do dia

---
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/exercise2.js
@@ -0,0 +1,3 @@
+// Exercícios - agora, a prática
+// Crie uma função sum que dado um número ilimitado de parâmetros retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6) o número 15 deve ser retornado.
+// escreva sum abaixo
