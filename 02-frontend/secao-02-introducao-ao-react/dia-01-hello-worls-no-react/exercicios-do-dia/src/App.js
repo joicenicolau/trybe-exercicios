0c997f7f23a1b631ec94a0f2059671e074036c17
Joice Nicolau <joice_nicolau@hotmail.com>
2022-11-22 20:33:23 -0300

    exercícios do dia

---
+++ 02-frontend/secao-02-introducao-ao-react/dia-01-hello-worls-no-react/exercicios-do-dia/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
