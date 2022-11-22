c53084fa4bba7c96d567ed87e7934bef3ad31767
Joice Nicolau <joice_nicolau@hotmail.com>
2022-11-22 15:30:04 -0300

    exercicios de fixaçao - 2

---
+++ 02-frontend/secao-02-introducao-ao-react/dia-01-hello-worls-no-react/exercicios-de-fixacao/src/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return (
+            <section>
+                <div>
+                    <h1>
+                        Joice
+                    </h1>
+                    <p>
+                    Sou formada e trabalho, desde os meus 21 anos de idade, mas a paixão por Tecnologia me levou até a Trybe, uma escola que ensina a programar, a aprender e a trabalhar na área de Desenvolvimento Web. Trabalho e acredito em realizações que possam acrescentar um resultado positivo as pessoas. 
+                    </p>
+                </div>
+                <div>
+                    <h2>
+                        Minhas Habilidades
+                    </h2>
+                    <ul>
+                     <li>JavaScript</li>
+                     <li>React</li>
+                     <li>HTML</li>
+                     <li>CSS</li>
+                    </ul>
+                </div>
+            </section>
+         
+        )
+    }
+}
+
+export default About;
