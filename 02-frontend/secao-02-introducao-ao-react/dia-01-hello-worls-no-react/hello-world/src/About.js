98e78adcc885b0667b75edf9b859a96c5928e6ce
Joice Nicolau <joice_nicolau@hotmail.com>
2022-11-22 15:15:20 -0300

    Exercício de fixação

---
+++ 02-frontend/secao-02-introducao-ao-react/dia-01-hello-worls-no-react/hello-world/src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <section>
+                <h1>
+                    Hello, wolrd!
+                </h1>
+            </section>
+        )
+    }
+}
+
+export default About;
