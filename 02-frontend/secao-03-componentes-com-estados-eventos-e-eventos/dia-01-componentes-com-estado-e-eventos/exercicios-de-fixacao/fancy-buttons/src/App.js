b03850c690de479cb25d067f850cb00da7b6d9e3
Joice Nicolau <joice_nicolau@hotmail.com>
2022-11-29 16:05:33 -0300

    exercício de fixação - parte 2

--- 02-frontend/secao-03-componentes-com-estados-eventos-e-eventos/dia-01-componentes-com-estado-e-eventos/exercicios-de-fixacao/fancy-buttons/src/App.js
+++ 02-frontend/secao-03-componentes-com-estados-eventos-e-eventos/dia-01-componentes-com-estado-e-eventos/exercicios-de-fixacao/fancy-buttons/src/App.js
@@ -2,25 +2,35 @@
 import './App.css';
 import React from 'react';
 
-function clickOne () {
-  console.log('Click 1');
-}
-
-function clickTwo () {
-  console.log('Click 2');
-}
-
-function clickThree () {
-  console.log('Click 3');
-}
-
 class App extends React.Component {
+    constructor() {
+      super()
+      this.clickOne = this.clickOne.bind(this)
+      this.clickTwo = this.clickTwo.bind(this)
+      this.clickThree = this.clickThree.bind(this)
+    }
+
+    clickOne () {
+      console.log(this);
+      console.log('Click 1');
+    }
+    
+    clickTwo () {
+      console.log(this);
+      console.log('Click 2');
+    }
+    
+    clickThree () {
+      console.log(this);
+      console.log('Click 3');
+    }
+
     render() {
     return (
       <div>
-        <button onClick={ clickOne }>Botão 1</button>
-        <button onClick={ clickTwo }>Botão 2</button>
-        <button onClick={ clickThree }>Botão 3</button>
+        <button onClick={ this.clickOne }>Botão 1</button>
+        <button onClick={ this.clickTwo }>Botão 2</button>
+        <button onClick={ this.clickThree }>Botão 3</button>
       </div>
     );
   }
